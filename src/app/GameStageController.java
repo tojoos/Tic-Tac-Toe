6479f0950c4ca0f604ff64e8a67186f9011800d2
ToJoos <joosx2000@gmail.com>
2021-07-14 12:17:56 +0200

    Decorations + crossing lines at the end of the game. Easy difficulty AI.

--- src/app/GameStageController.java
+++ src/app/GameStageController.java
@@ -1,5 +1,6 @@
 package app;
 
+import javafx.animation.PauseTransition;
 import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.SimpleBooleanProperty;
 import javafx.fxml.FXML;
@@ -14,9 +15,9 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
-import javafx.scene.text.Text;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.io.IOException;
 import java.net.URL;
@@ -25,13 +26,18 @@ import java.util.Arrays;
 import java.util.Objects;
 import java.util.ResourceBundle;
 
+/*
+*  TRYB JEDNOOSOBOWY VS AI!*/
+
 public class GameStageController implements Initializable {
 
     private BooleanProperty booleanProperty1 = new SimpleBooleanProperty(true);
     private BooleanProperty booleanProperty2 = new SimpleBooleanProperty(false);
+    private boolean isGameFinished = false;
     private ArrayList<ImageView> Xs;
     private ArrayList<ImageView> Os;
     private ArrayList<Button> tileList;
+    private int moveIter = 0;
 
     private char [][] gameBoard = new char [3][3];
 
@@ -39,7 +45,7 @@ public class GameStageController implements Initializable {
     private GridPane O_grid, X_grid, tileGrid;
 
     @FXML
-    private Group O_TurnGroup, X_TurnGroup, verticalLineGroup, horizontalLineGroup, crossLineGroup;
+    private Group O_TurnGroup, X_TurnGroup, newGameGroup, diffGroup;
 
     @FXML
     private ImageView X_0, X_1, X_2, X_3, X_4, X_5, X_6, X_7, X_8,
@@ -47,6 +53,12 @@ public class GameStageController implements Initializable {
 
     @FXML
     private Button tileButton0, tileButton1, tileButton2, tileButton3, tileButton4, tileButton5, tileButton6, tileButton7, tileButton8;
+
+    @FXML
+    private ImageView horzLine1, horzLine2, horzLine3, vertLine1, vertLine2, vertLine3, crossLine1, crossLine2;
+
+    @FXML
+    private Label onePlayerButton, twoPlayersButton, easyDiff, hardDiff;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -85,12 +97,15 @@ public class GameStageController implements Initializable {
                     booleanProperty1.setValue(!booleanProperty1.get());
                     booleanProperty2.setValue(!booleanProperty2.get());
 
-                    if(!checkGameStatus('X') && !checkGameStatus('O')) {
-                        if (isGameBoardFull()) {
-                            showEndOfTheGameStage('T');
+                    if(!isGameFinished) {
+                        if (!checkGameStatus('X') && !checkGameStatus('O')) {
+                            if (isGameBoardFull()) {
+                                showEndOfTheGameStage('T');
+                                for (Button b : tileList)
+                                    b.setDisable(true);
+                            }
                         }
                     }
-
                 });
             }
             i++;
@@ -117,7 +132,31 @@ public class GameStageController implements Initializable {
            (gameBoard[0][0] == XorO && gameBoard[1][0] == XorO && gameBoard[2][0] == XorO) ||
            (gameBoard[0][1] == XorO && gameBoard[1][1] == XorO && gameBoard[2][1] == XorO) ||
            (gameBoard[0][2] == XorO && gameBoard[1][2] == XorO && gameBoard[2][2] == XorO)) {
-            showEndOfTheGameStage(XorO);
+
+            for(Button b : tileList)
+                b.setDisable(true);
+
+            isGameFinished = true;
+
+            if(gameBoard[0][0] == XorO && gameBoard[1][1] == XorO && gameBoard[2][2] == XorO)
+                crossLine1.setVisible(true);
+            if(gameBoard[0][2] == XorO && gameBoard[1][1] == XorO && gameBoard[2][0] == XorO)
+                crossLine2.setVisible(true);
+            if(gameBoard[0][0] == XorO && gameBoard[0][1] == XorO && gameBoard[0][2] == XorO)
+                horzLine1.setVisible(true);
+            if(gameBoard[1][0] == XorO && gameBoard[1][1] == XorO && gameBoard[1][2] == XorO)
+                horzLine2.setVisible(true);
+            if(gameBoard[2][0] == XorO && gameBoard[2][1] == XorO && gameBoard[2][2] == XorO)
+                horzLine3.setVisible(true);
+            if(gameBoard[0][0] == XorO && gameBoard[1][0] == XorO && gameBoard[2][0] == XorO)
+                vertLine1.setVisible(true);
+            if(gameBoard[0][1] == XorO && gameBoard[1][1] == XorO && gameBoard[2][1] == XorO)
+                vertLine2.setVisible(true);
+            if(gameBoard[0][2] == XorO && gameBoard[1][2] == XorO && gameBoard[2][2] == XorO)
+                vertLine3.setVisible(true);
+            PauseTransition pt1 = new PauseTransition(Duration.millis(500));
+            pt1.playFromStart();
+            pt1.setOnFinished(e -> showEndOfTheGameStage(XorO));
             return true;
         } else {
             return false;
@@ -152,6 +191,15 @@ public class GameStageController implements Initializable {
                 }
             }
         }
+
+        vertLine1.setVisible(false);
+        vertLine2.setVisible(false);
+        vertLine3.setVisible(false);
+        horzLine1.setVisible(false);
+        horzLine2.setVisible(false);
+        horzLine3.setVisible(false);
+        crossLine1.setVisible(false);
+        crossLine2.setVisible(false);
     }
 
     private void initializeLists() {
@@ -172,7 +220,74 @@ public class GameStageController implements Initializable {
     }
 
     @FXML
-    private Label onePlayerButton, twoPlayersButton;
+    private void onTwoPlayersButtonClicked() {
+        newGameGroup.setVisible(false);
+        O_TurnGroup.visibleProperty().bind(booleanProperty1);
+        X_TurnGroup.visibleProperty().bind(booleanProperty2);
+        tileGrid.setDisable(false);
+    }
+
+    @FXML
+    private void onOnePlayerButtonClicked() {
+        newGameGroup.setVisible(false);
+        diffGroup.setVisible(true);
+    }
+
+    private void stupidAICode(){
+            int random = (int) (Math.random() * 8);
+            while (tileList.get(random).isDisabled()) {
+                random = (int) (Math.random() * 8);
+            }
+
+            Xs.get(random).setVisible(true);
+            if (random > 5) {
+                gameBoard[2][random - 6] = 'X';
+            } else if (random > 2) {
+                gameBoard[1][random - 3] = 'X';
+            } else {
+                gameBoard[0][random] = 'X';
+            }
+            tileList.get(random).setDisable(true);
+            booleanProperty1.setValue(!booleanProperty1.get());
+            booleanProperty2.setValue(!booleanProperty2.get());
+
+            if (!checkGameStatus('X') && !checkGameStatus('O')) {
+                if (isGameBoardFull()) {
+                    showEndOfTheGameStage('T');
+                    for (Button b : tileList)
+                        b.setDisable(true);
+                }
+            }
+    }
+
+    @FXML
+    private void onEasyDiffButtonClicked() {
+        startAnAIGame("EASY");
+    }
+
+    @FXML
+    private void onHardDiffButtonClicked() {
+        startAnAIGame("HARD");
+    }
+
+    private void startAnAIGame(String diff){
+        diffGroup.setVisible(false);
+        O_TurnGroup.visibleProperty().bind(booleanProperty1);
+        X_TurnGroup.visibleProperty().bind(booleanProperty2);
+        X_TurnGroup.visibleProperty().addListener(e -> {
+            if(X_TurnGroup.isVisible()) {
+                if (moveIter < 4) {
+                    if(diff.equals("EASY"))
+                        stupidAICode();
+                    else;
+
+                }
+                moveIter++;
+            }
+        });
+        tileGrid.setDisable(false);
+
+    }
 
     @FXML
     private void onTwoPlayersButtonEntered() {
@@ -195,33 +310,22 @@ public class GameStageController implements Initializable {
     }
 
     @FXML
-    private Text newGameText;
-
-    @FXML
-    private void onTwoPlayersButtonClicked() {
-        hideNewGameControls();
-        O_TurnGroup.visibleProperty().bind(booleanProperty1);
-        X_TurnGroup.visibleProperty().bind(booleanProperty2);
-        tileGrid.setDisable(false);
+    private void onEasyDiffButtonEntered() {
+        easyDiff.underlineProperty().setValue(true);
     }
 
     @FXML
-    private void onOnePlayerButtonClicked() {
-        hideNewGameControls();
-        O_TurnGroup.visibleProperty().bind(booleanProperty1);
-        X_TurnGroup.visibleProperty().bind(booleanProperty2);
-        tileGrid.setDisable(false);
+    private void onEasyDiffButtonExited() {
+        easyDiff.underlineProperty().setValue(false);
     }
 
-    private void hideNewGameControls() {
-        onePlayerButton.setVisible(false);
-        twoPlayersButton.setVisible(false);
-        newGameText.setVisible(false);
+    @FXML
+    private void onHardDiffButtonEntered() {
+        hardDiff.underlineProperty().setValue(true);
     }
 
-    private void showNewGameControls() {
-        onePlayerButton.setVisible(true);
-        twoPlayersButton.setVisible(true);
-        newGameText.setVisible(true);
+    @FXML
+    private void onHardDiffButtonExited() {
+        hardDiff.underlineProperty().setValue(false);
     }
 }
